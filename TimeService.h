bc224633b079a8ecd9e4b1a8c1c4ccdd35c16aec
huynhphi1510 <huynhphi151007@gmail.com>
2026-06-22 17:13:28 +0700

    them comment de xay dung class

---
+++ TimeService.h
@@ -0,0 +1,5 @@
+//ko co thuoc tinh
+//gom cac static method: getCurrentDate(string) ham nay se lay  ngay thuc te khi bat dau lam bai theo dinh dang DD/MM/YYYY
+
+//getCurrentTime(string) lay thoi gan thuc te theo dinh dang hour:minute:secondd,
+//calculateElapsedSeconds(int) ham nay gom 2tham so time_t start va time_t end de tinh thoi gian lam bai cua thi sinh ham nay se duoc goi trong ExamResult
