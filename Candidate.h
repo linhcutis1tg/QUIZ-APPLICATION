84b1fac2b8ecc77315d408ae8917e7e238ae86fe
quachphusang0509 <quachphusang.050907@gmail.com>
2026-06-22 21:49:27 +0700

    Cập nhật class Candidate.h

--- Candidate.h
+++ Candidate.h
@@ -1,2 +1,27 @@
 // //viet class candidate gom cac thuoc tinh: id (string), name(string) 
 // cac phuong thuc: constructor, ham inputinfo() de nhap thong tin thi sinh
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Candidate {
+private:
+    string id;
+    string name;
+
+public:
+    // Constructor
+    Candidate(string id = "", string name = "");
+
+    // Hàm nhập thông tin
+    void inputInfo();
+
+    // Getter
+    string getId() const;
+    string getName() const;
+
+    // Nạp chồng toán tử
+    friend istream& operator>>(istream& in, Candidate& c);
+    friend ostream& operator<<(ostream& out, const Candidate& c);
+};
