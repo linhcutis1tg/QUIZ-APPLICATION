5afdbf0437b91a397ccaf303d7872def02559e70
quachphusang0509 <quachphusang.050907@gmail.com>
2026-06-26 20:33:12 +0700

    Thêm điều kiện và operator

--- Candidate.h
+++ Candidate.h
@@ -1,28 +1,102 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <cctype>
+
 using namespace std;
 
-class Candidate {
+class Candidate
+{
 private:
     string id;
     string name;
 
-public:
-    Candidate(string i = "", string n = "") : id(i), name(n) {}
-     
-    // operator overload nhập
-    void inputInfo()
+    // Kiểm tra mã sinh viên
+    bool isValidID(string id)
     {
-        cout << "Nhap ma sinh vien: ";
-        cin >> ws;
-        getline(cin, id);
+        if (id.length() < 8 || id.length() > 12)
+        {
+            return false;
+        }
 
-        cout << "Nhap ho ten: ";
-        getline(cin, name);
+        for (char c : id)
+        {
+            if (!isalnum(c))  //isalnum() hàm có sẵn của C++ kiểm tra các Ký tự có phải là số ko
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    string getId() const { return id; }
-    string getName() const { return name; } 
-    // setname() 
+    // Kiểm tra họ tên
+    bool isValidName(string name)
+    {
+        for (char c : name)
+        {
+            if (!isalpha(c) && c != ' ') //isalpha() hàm có sẵn của C++ kiểm tra các Ký tự có phải là chữ ko
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+public:
+    Candidate(string i = "", string n = "")
+        : id(i), name(n)
+    {
+    }
+
+    friend istream& operator>>(istream& in, Candidate& c)
+    {
+        // Nhập mã sinh viên
+        do
+        {
+            cout << "Enter student ID number: ";
+            in >> ws;
+            getline(in, c.id);
+
+            if (!c.isValidID(c.id))
+            {
+                cout << "Invalid student ID! Please enter it again.\n";
+            }
+
+        } while (!c.isValidID(c.id));
+
+        // Nhập họ tên
+        do
+        {
+            cout << "Enter full name: ";
+            getline(in, c.name);
+
+            if (!c.isValidName(c.name))
+            {
+                cout << "Invalid full name! Please enter it again..\n";
+            }
+
+        } while (!c.isValidName(c.name));
+
+        return in;
+    }
+
+    friend ostream& operator<<(ostream& out, const Candidate& c)
+    {
+        out << "Student ID: " << c.id << endl;
+        out << "Full name: " << c.name << endl;
+
+        return out;
+    }
+
+    string getId() const
+    {
+        return id;
+    }
+
+    string getName() const
+    {
+        return name;
+    }
 };
