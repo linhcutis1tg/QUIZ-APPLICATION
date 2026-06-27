9a1a0d8bcae18d9e29b55803780567da3b8f60cc
quachphusang0509 <quachphusang.050907@gmail.com>
2026-06-28 05:17:58 +0700

    đoi

--- Candidate.h
+++ Candidate.h
@@ -71,7 +71,7 @@ istream& operator>>(istream& in, Candidate& c)
 {
     // Vòng lặp kiểm tra điều kiện mã sinh viên
     do {
-        cout << "Enter student ID number (8-12 alphanumeric characters): ";
+        cout << "\n👤 Student ID :";
         in >> ws; // Xóa khoảng trắng và ký tự xuống dòng còn sót trong bộ đệm
         getline(in, c.id);
         if (!c.isValidID(c.id)) {
@@ -81,7 +81,7 @@ istream& operator>>(istream& in, Candidate& c)
 
     // Vòng lặp kiểm tra điều kiện họ tên
     do {
-        cout << "Enter full name (letters only): ";
+        cout << "👤 Full Name  :\n";
         getline(in, c.name);
         if (!c.isValidName(c.name)) {
             cout << "Invalid full name! Please try again.\n";
