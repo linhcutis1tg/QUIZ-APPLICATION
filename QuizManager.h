e74f47e8a25a590e83f5b2e0e55faf99f934653a
huynhphi1510 <huynhphi151007@gmail.com>
2026-06-27 11:20:02 +0700

    nhap tt truoc chon chu de sau

--- QuizManager.h
+++ QuizManager.h
@@ -41,11 +41,14 @@ QuizManager::~QuizManager()
 
  bool QuizManager::loadQuestionFromFile(string filename) // code đọc lại file input
 {
+    // Vẽ khung giao diện trang trí bắt mắt lúc mở app giống video mẫu
     cout << "*********************************\n";
     cout << "* Quiz Program                  *\n";
     cout << "* Date: " << TimeService::getCurrentDate() << "              *\n"; // Gọi hàm lấy ngày tự động
     //cout << "* Number of questions: " << totalQuestions << "        *\n";
     cout << "*********************************\n";
+    cout << "Please enter your information!\n";
+    cin >> currentcandidate;
     char choice;
     // Lap chon mon hoc
     while (true) {
@@ -128,12 +131,9 @@ QuizManager::~QuizManager()
 }
 void QuizManager::startExam() {
 
-    // Vẽ khung giao diện trang trí bắt mắt lúc mở app giống video mẫu
     
-    cout << "Please enter your information!\n";
-
     
-    cin >> currentcandidate;
+   
     cout << "Press enter to start...";
     cin.get();
 
